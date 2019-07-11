32f29ee2b6629dd62c3a9bfe1c8b7d83373b4442
Sondre Eikanger Kvalø <zapodot@gmail.com>
2019-07-11 16:19:56 +0200

    Fix tests..?

--- test/sut/test.sh
+++ test/sut/test.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-wget -q -O- http://prometheus:9090/api/v1/label/__name__/values | grep mssql\_deadlocks
+sleep 10
+echo "Calling Prometheus HTTP API"
+if wget -q -O- 'http://prometheus:9090/api/v1/label/__name__/values' | jq '.data[] | select(. == "mssql_deadlocks")'; then
+    echo "Tests passed"
+    exit 0
+else
+    echo "Test failed"
+    exit 2
+fi
